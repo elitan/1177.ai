21356dc737f515e0b23bf508e838c5e73c27d667
Johan Eliasson <johan@eliasson.me>
2023-11-01 21:20:39 +0100

    setup

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "standalone",
+  experimental: {
+    scrollRestoration: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
