88b3b67150b764c67192bdd1da57deda1856d2f2
Johan Eliasson <johan@eliasson.me>
2023-11-01 08:18:13 +0100

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import PlausibleProvider from "next-plausible";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider
+      domain='1177.ai'
+      customDomain='https://analytics.eliasson.me'
+      selfHosted={true}
+      enabled={true}
+    >
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,14 @@ export default function Home() {
     >
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] text-5xl ">
-        1177.ai - the future of health is incoming.
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] ">
+        <div>
+          <div className='text-5xl'>1177.ai - The future of health.</div>
+          {/* <div className='mt-4'>
+            <a href=''>LinkedIn</a>
+            <a href=''>X</a>
+          </div> */}
+        </div>
       </div>
 
       <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'></div>
